fe4b294b120a1d327b74ee3a7e76665a506726ec
Toujou Aya <gamevnlc1591@gmail.com>
2018-03-01 02:13:41 +0700

    first

---
+++ test.py
@@ -0,0 +1,40 @@
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
+
+print("Hello World")
+
+print(x)
+
+myfloat = 1.2
+print(myfloat)
+
+myfloat = float(1.2)
+print(myfloat)
+
+mystring = 'hi'
+print(mystring)
+
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "hello"
+world = "world"
+helloWorld = hello + " " + world
+print(helloWorld)
+
+a, b = 3, 4
+print(a, b)
+
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+
+print(mylist)
+
+for x in mylist:
+    print(x)
